d0281335ed3e7e678670977ef716cd9849b2b4be
Rintaro Itokawa <aminokacho@gmail.com>
2021-01-16 02:38:11 +0900

    Add files via upload

---
+++ report-7_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    const double three=3.0;
+    int x;
+
+    for(x=2;x<10;x++){
+        cout<<pow(three,x)<<endl;
+    }
+}
