f419ee6749969ea9d04c807d2a900d6887f1b1bf
Rintaro Itokawa <aminokacho@gmail.com>
2021-01-17 03:41:38 +0900

    Add files via upload

---
+++ report-9_4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void printArray(double a[],int N){
+    for(int i=0;i<N;i++){
+        cout<<a[i]<<(i<N-1?",":"\n");
+    }
+}
+
+void sort(double b[],int N){
+    for(int c=0;c<=N-1;c++){
+        for(int d=c+1;d<N;d++){
+            if(b[c]<b[d]){
+                double ans=b[d];
+                b[d]=b[c];
+                b[c]=ans;
+            }
+        }
+    }
+}
+
+int main(){
+    const int maxn=10;
+    double v[maxn];
+    for(int i=0;i<maxn;i++){
+        cin>>v[i];
+    }
+    printArray(v,maxn);
+    sort(v,maxn);
+    printArray(v,maxn);
+}
