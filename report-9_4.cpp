17ad02989957732f2b48d9c7109a8538efa94816
Rintaro Itokawa <aminokacho@gmail.com>
2021-01-17 03:46:33 +0900

    Update report-9_4.cpp

--- report-9_4.cpp
+++ report-9_4.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
+void printArray(double a[],int N);
+
+void sort(double b[],int N);
+
+int main(){
+    const int maxn=10;
+    double v[maxn];
+    for(int i=0;i<maxn;i++){
+        cin>>v[i];
+    }
+    printArray(v,maxn);
+    sort(v,maxn);
+    printArray(v,maxn);
+}
+
 void printArray(double a[],int N){
     for(int i=0;i<N;i++){
         cout<<a[i]<<(i<N-1?",":"\n");
@@ -17,15 +32,4 @@ void sort(double b[],int N){
             }
         }
     }
-}
-
-int main(){
-    const int maxn=10;
-    double v[maxn];
-    for(int i=0;i<maxn;i++){
-        cin>>v[i];
-    }
-    printArray(v,maxn);
-    sort(v,maxn);
-    printArray(v,maxn);
 }
