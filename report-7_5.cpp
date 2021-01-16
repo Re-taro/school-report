55a703a122eebda8604b03b468923caaf696f80f
Rintaro Itokawa <aminokacho@gmail.com>
2021-01-16 13:34:29 +0900

    Update report-7_5.cpp

--- report-7_5.cpp
+++ report-7_5.cpp
@@ -7,30 +7,22 @@ double ThetaToRadian(double theta);
 int main(){
     double theta=0;
     double radian=0;
-    double sin_theta;
-    double cos_theta;
-    double tan_theta;
+    double sin_theta=0;
+    double cos_theta=0;
+    double tan_theta=0;
     do{
         cin>>theta;
     }while(theta<0.0||theta>360);
     if(theta==0){
-        sin_theta=0;
         cos_theta=1;
-        tan_theta=0;
     }else if(theta==90){
         sin_theta=1;
-        cos_theta=0;
     }else if(theta==180){
-        sin_theta=0;
         cos_theta=-1;
-        tan_theta=0;
     }else if(theta==270){
         sin_theta-1;
-        cos_theta=0;
     }else if(theta==360){
-        sin_theta=0;
         cos_theta=1;
-        tan_theta=0;
     }else{
         sin_theta=sin(ThetaToRadian(theta));
         cos_theta=cos(ThetaToRadian(theta));
